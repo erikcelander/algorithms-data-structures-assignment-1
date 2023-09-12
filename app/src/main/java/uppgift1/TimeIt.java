175b0031c18fed8b5c67dfafc69be0e9b1dcfaf2
Erik Kroon Celander <ek223ur@student.lnu.se>
2023-09-12 10:50:23 +0200

    edit comments for clarity

--- app/src/main/java/uppgift1/TimeIt.java
+++ app/src/main/java/uppgift1/TimeIt.java
@@ -12,25 +12,26 @@ public class TimeIt {
     }
 
     long end = System.nanoTime();
-    return (end - start) / repetitions; // return avg per time in ns
+    return (end - start) / repetitions; // return avg per call in ns
   }
 
   public static void main(String[] args) {
 
-    // Example function to test 
+    // Example function to test the functionality
     Callable<Void> code = () -> {
-        int sum = 0;
-        for (int i = 0; i < 10000; i++) {
-            sum += i;
-        }
-        return null;  // returning null here in example function because I used Callable<Void>
+      int sum = 0;
+      for (int i = 0; i < 100; i++) {
+        sum += i;
+      }
+      return null; // returning null here in example function because I used Callable<Void>
     };
 
     try {
-        long avg = timeIt(code, 10000);
-        System.out.println("Average execution time of the 10000 times the code was executed is:  " + avg + " nanoseconds.");
+      long avg = timeIt(code, 10000);
+      System.out
+          .println("Average execution time of the 10000 times the code was executed is:  " + avg + " nanoseconds.");
     } catch (Exception e) {
-        e.printStackTrace();
+      e.printStackTrace();
     }
-}
+  }
 }
