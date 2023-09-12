24d81dc9d6e412021add1cd7e6b86f4011b3b78c
Erik Kroon Celander <ek223ur@student.lnu.se>
2023-09-12 10:30:39 +0200

    exchange runnable interface for callable interface to account for errors

--- app/src/main/java/uppgift1/TimeIt.java
+++ app/src/main/java/uppgift1/TimeIt.java
@@ -1,29 +1,36 @@
 package uppgift1;
 
+import java.util.concurrent.Callable;
+
 public class TimeIt {
 
-  public static long timeIt(Runnable code, int repetitions) {
+  public static <T> long timeIt(Callable<T> code, int repetitions) throws Exception {
     long start = System.nanoTime();
 
     for (int i = 0; i < repetitions; i++) {
-      code.run();
+      code.call();
     }
 
     long end = System.nanoTime();
-    return (end - start) / repetitions; // return avg per time
+    return (end - start) / repetitions; // return avg per time in ns
   }
 
   public static void main(String[] args) {
-    Runnable code = () -> {
-      int sum = 0;
-      for (int i = 0; i < 100; i++) {
-        sum += i;
-      }
+
+    // Example function to test 
+    Callable<Void> code = () -> {
+        int sum = 0;
+        for (int i = 0; i < 10000; i++) {
+            sum += i;
+        }
+        return null;  // returning null here in example function because I used Callable<Void>
     };
 
-    long avg = timeIt(code, 10000);
-
-
-    System.out.println("Average execution time: " + avg + " nanoseconds");
-  }
+    try {
+        long avg = timeIt(code, 10000);
+        System.out.println("Average execution time of the 10000 times the code was executed is:  " + avg + " nanoseconds.");
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}
 }
