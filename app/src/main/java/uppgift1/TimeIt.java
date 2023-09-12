6bf002834c6a9f296beb44bc2cff6bfca2d10725
Erik Kroon Celander <ek223ur@student.lnu.se>
2023-09-12 12:30:37 +0200

    refactor timeit to calculate the min, max and median as well

--- app/src/main/java/uppgift1/TimeIt.java
+++ app/src/main/java/uppgift1/TimeIt.java
@@ -1,18 +1,50 @@
 package uppgift1;
 
+import java.util.Arrays;
 import java.util.concurrent.Callable;
 
 public class TimeIt {
 
-  public static <T> long timeIt(Callable<T> code, int repetitions) throws Exception {
-    long start = System.nanoTime();
+  public static class TimingResult {
+    public final int repetitions;
+    public final long min;
+    public final long median;
+    public final long avg;
+    public final long max;
 
-    for (int i = 0; i < repetitions; i++) {
-      code.call();
+    public TimingResult(long min, long median, long avg, long max, int repetitions) {
+      this.repetitions = repetitions;
+      this.min = min;
+      this.median = median;
+      this.avg = avg;
+      this.max = max;
     }
 
-    long end = System.nanoTime();
-    return (end - start) / repetitions; // return avg per call in ns
+    @Override
+    public String toString() {
+      return "Min: " + min + " ns, Median: " + median + " ns, Avg: " + avg + " ns, Max: " + max + " ns." +
+          " This was calculated by executing the code " + repetitions + " times."; // Updated string to include reps
+    }
+  }
+
+  public static <T> TimingResult timeIt(Callable<T> code, int repetitions) throws Exception {
+    long[] times = new long[repetitions];
+
+    for (int i = 0; i < repetitions; i++) {
+      long start = System.nanoTime();
+      code.call();
+      long end = System.nanoTime();
+      times[i] = end - start;
+    }
+
+    Arrays.sort(times);
+    long total = Arrays.stream(times).sum();
+    long avg = total / repetitions;
+    long min = times[0];
+    long median = times[repetitions / 2];
+    long max = times[repetitions - 1];
+
+    return new TimingResult(min, median, avg, max, repetitions);
   }
 
   public static void main(String[] args) {
@@ -23,13 +55,13 @@ public class TimeIt {
       for (int i = 0; i < 100; i++) {
         sum += i;
       }
-      return null; // returning null here in example function because I used Callable<Void>
+      return null; // returning null here in the example lambda function because I used
+                   // Callable<Void>
     };
 
     try {
-      long avg = timeIt(code, 10000);
-      System.out
-          .println("Average execution time of the 10000 times the code was executed is:  " + avg + " nanoseconds.");
+      TimingResult res = timeIt(code, 10000);
+      System.out.println(res.toString());
     } catch (Exception e) {
       e.printStackTrace();
     }
