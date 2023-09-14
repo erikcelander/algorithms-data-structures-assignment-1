e5fdf65d7a21c53327b0110f2d9d503999e8c269
Erik Kroon Celander <ek223ur@student.lnu.se>
2023-09-14 13:33:21 +0200

    fix mistake in code

--- app/src/main/java/uppgift/ThreeSumBruteForce.java
+++ app/src/main/java/uppgift/ThreeSumBruteForce.java
@@ -1,6 +1,7 @@
 package uppgift;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ThreeSumBruteForce {
@@ -12,14 +13,27 @@ public class ThreeSumBruteForce {
       for (int j = 0; j < numbers.length; j++) {
         for (int k = 0; k < numbers.length; k++) {
           if (i == j || i == k || j == k) {
-            break;
-          } else if((i + j + k) == 0) {
-            res.add(new int[]{numbers[i], numbers[k], numbers[j]});
+            continue;
+
+          } 
+          
+          if ((numbers[i] + numbers[j] + numbers[k]) == 0) {
+            res.add(new int[] { numbers[i], numbers[k], numbers[j] });
           }
         }
       }
     }
 
     return res;
+  }
+
+  public static void main(String[] args) {
+    int[] nums = { -2, -1, 0, 1, 2 };
+    List<int[]> result = threeSum(nums);
+
+
+    for (int[] triplet : result) {
+      System.out.println(Arrays.toString(triplet));
+    }
   }
 }
