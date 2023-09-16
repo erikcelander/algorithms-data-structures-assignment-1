08bd544eee7700c415370016d89faf997601a2f0
Erik Kroon Celander <ek223ur@student.lnu.se>
2023-09-16 15:27:02 +0200

    improve duplicate logic

--- app/src/main/java/uppgift/ThreeSumCache.java
+++ app/src/main/java/uppgift/ThreeSumCache.java
@@ -10,12 +10,12 @@ public class ThreeSumCache {
   public static List<int[]> threeSum(int[] numbers) {
     List<int[]> res = new ArrayList<>();
 
-    Arrays.sort(numbers);
+    Arrays.sort(numbers); // sort to help find/avoid duplicates later
 
     for (int i = 0; i < numbers.length; i++) {
-      
-      // if we aren't at index 0 and the value is the same 
-      // as previous index, skip to avoid duplicates
+
+      // if we aren't at index 0 (because then there would be no previous index)
+      // and the value is the same as the previous index, skip to avoid duplicates
       if (i != 0 && numbers[i] == numbers[i - 1]) {
         continue;
       }
@@ -25,6 +25,12 @@ public class ThreeSumCache {
       Map<Integer, Integer> cache = new HashMap<>();
 
       for (int j = i + 1; j < numbers.length; j++) {
+
+        // if j has moved past i + 1 and the current value
+        // is the same as the previous, skip to avoid duplicates
+        if (j > i + 1 && numbers[j] == numbers[j - 1]) {
+          continue;
+        }
 
         int complement = target - numbers[j]; // the value of the last number for the sum to be 0
 
@@ -40,7 +46,7 @@ public class ThreeSumCache {
   }
 
   public static void main(String[] args) {
-    int[] nums = {-1, -1, 0, 1, 1};
+    int[] nums = {-4, -2, -2, -1, 0, 1, 2, 2, 3, 4};
 
     List<int[]> result = threeSum(nums);
 
