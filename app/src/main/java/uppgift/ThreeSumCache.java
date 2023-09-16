1d6e04ad78052c8546a004708d47347a987807f4
Erik Kroon Celander <ek223ur@student.lnu.se>
2023-09-16 15:06:48 +0200

    improve three sum cache algo by removing unneccessary operations

--- app/src/main/java/uppgift/ThreeSumCache.java
+++ app/src/main/java/uppgift/ThreeSumCache.java
@@ -10,18 +10,17 @@ public class ThreeSumCache {
   public static List<int[]> threeSum(int[] numbers) {
     List<int[]> res = new ArrayList<>();
 
+    Arrays.sort(numbers);
+
     for (int i = 0; i < numbers.length; i++) {
 
       int target = -numbers[i]; // the sum we want to find using the other two numbers
 
       Map<Integer, Integer> cache = new HashMap<>();
 
-      for (int j = 0; j < numbers.length; j++) {
-        if (i == j) {
-          continue; // skip the same number
-        }
+      for (int j = i + 1; j < numbers.length; j++) {
 
-        int complement = target - numbers[j];
+        int complement = target - numbers[j]; // the value of the last number for the sum to be 0
 
         if (cache.containsKey(complement)) {
           res.add(new int[] { numbers[i], numbers[j], complement });
